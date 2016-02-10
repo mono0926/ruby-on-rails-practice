f6afbbd67496e2e5329333f4f12bba0c060b3b12
Masayuki Ono <mono0926@gmail.com>
2016-02-10 11:53:00 +0900

    Add session

--- baukis/config/routes.rb
+++ baukis/config/routes.rb
@@ -50,6 +50,9 @@ Rails.application.routes.draw do
   # Example resource route within a namespace:
   namespace :staff do
     root 'top#index'
+    get 'login' => 'sessions#new', as: :login
+    post 'session' => 'sessions#create', as: :session
+    delete 'session' => 'sessions#destroy'
   end
 
   namespace :admin do
