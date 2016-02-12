51a645668a44c1d87bf56f4cebf9557d1b83bde4
Masayuki Ono <mono0926@gmail.com>
2016-02-12 11:58:27 +0900

    Add dashboard

--- baukis/config/routes.rb
+++ baukis/config/routes.rb
@@ -60,6 +60,7 @@ Rails.application.routes.draw do
     get 'login' => 'sessions#new', as: :login
     post 'session' => 'sessions#create', as: :session
     delete 'session' => 'sessions#destroy'
+    resources :staff_members
   end
 
   namespace :customer do
