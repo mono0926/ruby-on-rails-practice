67433e98fd0d4cdd7faa9cc3085e0326f9cab365
Masayuki Ono <mono0926@gmail.com>
2016-02-12 14:33:57 +0900

    Enhance routes

--- baukis/config/routes.rb
+++ baukis/config/routes.rb
@@ -51,15 +51,14 @@ Rails.application.routes.draw do
   namespace :staff do
     root 'top#index'
     get 'login' => 'sessions#new', as: :login
-    post 'session' => 'sessions#create', as: :session
-    delete 'session' => 'sessions#destroy'
+    resource :session, only: [ :create, :destroy ]
+    resources :account
   end
 
   namespace :admin do
     root 'top#index'
     get 'login' => 'sessions#new', as: :login
-    post 'session' => 'sessions#create', as: :session
-    delete 'session' => 'sessions#destroy'
+    resource :session, only: [ :create, :destroy ]
     resources :staff_members
   end
 
