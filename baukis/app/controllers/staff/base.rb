933c1552737f4ed1edabf697d1664d3325e641f7
Masayuki Ono <mono0926@gmail.com>
2016-02-12 20:56:19 +0900

    Add timeout

--- baukis/app/controllers/staff/base.rb
+++ baukis/app/controllers/staff/base.rb
@@ -26,4 +26,18 @@ class Staff::Base < ApplicationController
       redirect_to :staff_root
     end
   end
+
+  TIMEOUT = 60.minutes
+
+  def check_timeout
+    if current_staff_member
+      if session[:last_access_time] >= TIMEOUT.ago
+        session[:last_access_time] = Time.current
+      else
+        session.delete(:staff_member_id)
+        flash.alert = 'セッションがタイムアウトしました。'
+        redirect_to :staff_login
+      end
+    end
+  end
 end
